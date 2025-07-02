2f7f29aff6548a85339e0441b420d40cb0510094
Aniket Khambal <aniketocto@gamil.com>
2025-07-02 14:01:54 +0530

    importing thing to server js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(
@@ -16,12 +18,11 @@ app.use(
 
 connectDB();
 
-
 // Middleware
 app.use(express.json());
 
 // Routes
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 // app.use("/api/users", userRoutes.js)
 // app.use("/api/tasks", taskRoutes.js)
 // app.use("/api/reorts", reportsRoutes.js)
