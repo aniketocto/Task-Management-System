40f2acf941e08a2634e27c7961bfe62667c558ac
Aniket Khambal <aniketocto@gamil.com>
2025-07-07 13:09:13 +0530

    sidebar menu and helper data

--- frontend/src/components/layouts/SideMenu.jsx
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/userContext";
+import React, {useContext, useEffect, useState } from "react";
+import { SIDE_MENU_DATA, SIDE_MENU_USER_DATA } from "../../utils/data";
 
-const SideMenu = () => {
+const SideMenu = ({ activeMenu }) => {
+  const { user, clearUser } = useContext(UserContext);
+  const [sideMenuData, setSideMenuData] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleClick = (route) => {
+    if (route === "logout") {
+      handleLogout();
+      return;
+    }
+
+    navigate(route);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("taskManagerToken");
+    clearUser();
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    console.log(user);
+    if (user) {
+      setSideMenuData(
+        user?.role === "admin" || user?.role === "superAdmin"
+          ? SIDE_MENU_DATA
+          : SIDE_MENU_USER_DATA
+      );
+    }
+    return () => {};
+  }, [user]);
+
   return (
-    <div>SideMenu</div>
-  )
-}
+    <div className="w-64 h-[calc(100vh-61px)] bg-white border-r border-gray-500/40 sticky top-[61px] z-20">
+      <div className="flex flex-col items-center justify-center mb-7 pt-5">
+        <div className="">
+          <img
+            src={user?.profileImageUrl || ""}
+            alt="profile Image"
+            className="w-20 h-20 border border-gray-500/40 rounded-full object-contain"
+          />
+        </div>
+        {(user?.role === "admin" || user?.role === "superAdmin") && (
+          <div className="text-[10px] font-medium text-white bg-primary px-3 py-0.5 rounded mt-1">
+            {user.role === "admin" ? "Admin" : "SuperAdmin"}
+          </div>
+        )}
 
-export default SideMenu
+        <h5 className="text-gray-950 font-medium leading-6 mt-3">
+          {user?.name}
+        </h5>
+        <p className="text-[12px] text-gray-500"> {user?.department || ""} </p>
+        <p className="text-[12px] text-gray-500 mb-5"> {user?.email || ""} </p>
+
+        {sideMenuData.map((item, index) => (
+          <button
+            key={`menu_${index}`}
+            className={`w-full flex items-center gap-4 text-[15px] ${
+              activeMenu == item.label
+                ? "text-primary bg-linear-to-r from-blue-50/40 border-r-3"
+                : ""
+            } py-3 px-6 mb-3 cursor-pointer`}
+            onClick={() => handleClick(item.path)}
+          >
+            <item.icon className="text-xl" />
+            {item.label}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SideMenu;
---
+++ frontend/src/utils/data.js
@@ -0,0 +1,74 @@
+import {
+  LuClipboardCheck,
+  LuLayoutDashboard,
+  LuLogOut,
+  LuSquare,
+} from "react-icons/lu";
+
+export const SIDE_MENU_DATA = [
+  {
+    id: "01",
+    label: "Dashboard",
+    icon: LuLayoutDashboard,
+    path: "/admin/dashboard",
+  },
+  {
+    id: "02",
+    label: "Manage Tasks",
+    icon: LuClipboardCheck,
+    path: "/admin/tasks",
+  },
+  {
+    id: "03",
+    label: "Creaete Task",
+    icon: LuSquare,
+    path: "/admin/create-task",
+  },
+  {
+    id: "04",
+    label: "Team Members",
+    icon: LuClipboardCheck,
+    path: "/admin/users",
+  },
+  {
+    id: "05",
+    label: "Logout",
+    icon: LuLogOut,
+    path: "logout",
+  },
+];
+
+export const SIDE_MENU_USER_DATA = [
+  {
+    id: "01",
+    label: "Dashboard",
+    icon: LuLayoutDashboard,
+    path: "/user/dashboard",
+  },
+  {
+    id: "02",
+    label: "My Tasks",
+    icon: LuClipboardCheck,
+    path: "/user/tasks",
+  },
+  {
+    id: "03",
+    label: "Logout",
+    icon: LuLogOut,
+    path: "logout",
+  },
+]
+
+export const PRIORITY_OPTIONS = [
+  { value: "high", label: "High" },
+  { value: "medium", label: "Medium" },
+  { value: "low", label: "Low" },
+];
+
+export const STATUS_OPTIONS = [
+  { value: "new", label: "New" },
+  { value: "delayed", label: "Delayed" },
+  { value: "pending", label: "Pending" },
+  { value: "in-progress", label: "In Progress" },
+  { value: "completed", label: "Completed" },
+];
