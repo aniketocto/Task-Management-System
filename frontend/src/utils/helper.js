6c5c4a6aadbf66c7f9750388abd0030f35f198bc
Aniket Khambal <aniketocto@gamil.com>
2025-07-07 13:23:30 +0530

    greeting helper function

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -12,6 +12,23 @@ export const validatePassword = (password) => {
   const isLongEnough = password.length >= 6;
 
   return (
-    hasUppercase && hasLowercase && hasDigit /* && hasSpecialChar */ && isLongEnough
+    hasUppercase &&
+    hasLowercase &&
+    hasDigit /* && hasSpecialChar */ &&
+    isLongEnough
   );
+};
+export const getGreeting = () => {
+  const now = new Date();
+  const hour = now.getHours();
+
+  if (hour >= 5 && hour < 12) {
+    return "Good Morning";
+  } else if (hour >= 12 && hour < 17) {
+    return "Good Afternoon";
+  } else if (hour >= 17 && hour < 21) {
+    return "Good Evening";
+  } else {
+    return "Good Night";
+  }
 };
