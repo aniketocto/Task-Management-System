bd0ac08ede6fcc93ccc53c3064f3a7ccb25b02e7
Aniket Khambal <aniketocto@gamil.com>
2025-07-08 12:47:07 +0530

    setting up task managing page

--- frontend/src/pages/Admin/ManageTask.jsx
+++ frontend/src/pages/Admin/ManageTask.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useEffect, useState } from "react";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const ManageTask = () => {
-  return (
-    <div>ManageTask</div>
-  )
-}
+  const [allTask, setAllTask] = useState([]);
+  const [tabs, setTabs] = useState([]);
+  const [filterStatus, setFilterStatus] = useState("All");
+  const navigate = useNavigate();
 
-export default ManageTask
+  const getAllTasks = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS, {
+        params: {
+          status: filterStatus === "All" ? "" : filterStatus,
+        },
+      });
+
+      setAllTask(response.data?.tasks?.length > 0 ? response.data?.tasks : []);
+
+      // Map StatusSummary data with fixed label
+      const statusSummary = response.data?.statusSummary || {};
+      const statusArray = [
+        { label: "All", count: statusSummary?.all || 0 },
+        { label: "New", count: statusSummary?.new || 0 },
+        { label: "In Progress", count: statusSummary?.inProgress || 0 },
+        { label: "Completed", count: statusSummary?.completed || 0 },
+        { label: "Pending", count: statusSummary?.pending || 0 },
+        { label: "Delayed", count: statusSummary?.delayed || 0 },
+      ];
+
+      setTabs(statusArray);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  };
+
+  const handleClick = (taskData) => {
+    navigate("/admin/create-task", { state: { taskId: taskData } });
+  };
+
+  const handleDownloadTaskReport = () => {};
+
+  useEffect(() => {
+    getAllTasks(filterStatus);
+    return () => {};
+  }, [filterStatus]);
+
+  return (
+    <DashboardLayout activeMenu="Manage Tasks">
+      <div className="my-5"></div>
+    </DashboardLayout>
+  );
+};
+
+export default ManageTask;
