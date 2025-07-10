ec9f5dd6ea96a2424c12369fefff6f289d1ca2ae
Aniket Khambal <aniketocto@gamil.com>
2025-07-10 16:03:17 +0530

    created notification model

---
+++ backend/models/Notifcation.js
@@ -0,0 +1,30 @@
+const { is } = require("@react-three/fiber/dist/declarations/src/core/utils");
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    taskId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Task",
+      default: null,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Notification", notificationSchema);
