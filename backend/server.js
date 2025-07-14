b12efdf92587790e0abab87941b6da4305578280
Aniket Khambal <aniketocto@gmail.com>
2025-07-14 20:28:09 +0530

    modified url

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", // 🔧 Vite dev server
-      "https://unstoppablecrm.vercel.app/"
+      "https://unstoppablecrm.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
