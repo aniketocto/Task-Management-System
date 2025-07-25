c483a4d8ca2feea25da84171c71d43c45ad58870
Aniket Khambal <aniketocto@gmail.com>
2025-07-25 13:34:15 +0530

    added user serch in select user

--- frontend/src/components/Inputs/SelectUsers.jsx
+++ frontend/src/components/Inputs/SelectUsers.jsx
@@ -9,6 +9,7 @@ const SelectUsers = ({ selectedUsers, setSelectedUsers }) => {
   const [allUsers, setAllUsers] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [tempSelectedUsers, setTempSelectedUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const getAllUser = async () => {
     try {
@@ -52,10 +53,10 @@ const SelectUsers = ({ selectedUsers, setSelectedUsers }) => {
   }, [selectedUsers]);
 
   useEffect(() => {
-  if (isModalOpen) {
-    setTempSelectedUsers(selectedUsers);
-  }
-}, [isModalOpen, selectedUsers]);
+    if (isModalOpen) {
+      setTempSelectedUsers(selectedUsers);
+    }
+  }, [isModalOpen, selectedUsers]);
 
   return (
     <div className="space-y-4 mt-2">
@@ -76,32 +77,43 @@ const SelectUsers = ({ selectedUsers, setSelectedUsers }) => {
         onClose={() => setIsModalOpen(false)}
         title="Select Members"
       >
+        <input
+          type="text"
+          placeholder="Search by name"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full mb-3 p-2 text-sm border border-gray-300 rounded"
+        />
         <div className="sapce-y-4 h-[60vh] overflow-y-auto">
-          {allUsers.map((user) => (
-            <div
-              key={user._id}
-              className="flex items-center gap-4 p-3 border-b border-gray-200 cursor-pointer"
-              onClick={() => toggleUserSelection(user._id)}
-            >
-              <img
-                src={user.profileImageUrl}
-                alt={user.name}
-                className="w-10 h-10 rounded-full object-contain"
-              />
-              <div className="flex-1">
-                <p className="font-medium text-gray-800">{user.name}</p>
-                <p className="text-[13px] text-gray-600">{user.department}</p>
-              </div>
+          {allUsers
+            .filter((user) =>
+              user.name.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .map((user) => (
+              <div
+                key={user._id}
+                className="flex items-center gap-4 p-3 border-b border-gray-200 cursor-pointer"
+                onClick={() => toggleUserSelection(user._id)}
+              >
+                <img
+                  src={user.profileImageUrl}
+                  alt={user.name}
+                  className="w-10 h-10 rounded-full object-contain"
+                />
+                <div className="flex-1">
+                  <p className="font-medium text-gray-800">{user.name}</p>
+                  <p className="text-[13px] text-gray-600">{user.department}</p>
+                </div>
 
-              <input
-                type="checkbox"
-                checked={tempSelectedUsers.includes(user._id)}
-                readOnly
-                className="w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded-sm outline-none"
-                onClick={(e) => e.stopPropagation()} // optional: if you still want to prevent bubbling
-              />
-            </div>
-          ))}
+                <input
+                  type="checkbox"
+                  checked={tempSelectedUsers.includes(user._id)}
+                  readOnly
+                  className="w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded-sm outline-none"
+                  onClick={(e) => e.stopPropagation()} // optional: if you still want to prevent bubbling
+                />
+              </div>
+            ))}
         </div>
 
         <div className="flex justify-end gap-4 pt-4">
