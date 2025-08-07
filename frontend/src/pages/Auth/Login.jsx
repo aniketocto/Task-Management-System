26246120da762f23bbacd1f64f229e352715eaa7
Aniket Khambal <aniketocto@gmail.com>
2025-08-07 16:41:42 +0530

    asd

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -116,7 +116,7 @@ const Login = () => {
         </p>
 
         <form onSubmit={handleLogin} className="mt-8">
-          <Input
+          {/* <Input
             value={email}
             onChange={({ target }) => setEmail(target.value)}
             label="Email Address"
@@ -129,7 +129,7 @@ const Login = () => {
             label="Password"
             placeholder="Password"
             type="password"
-          />  
+          />   */}
 
           {/* {Array.isArray(error) && error.length > 0 && (
             <ul className="text-red-500 text-sm pb-2.5">
@@ -142,12 +142,12 @@ const Login = () => {
             onSuccess={handleGoogleSuccess}
             onError={() => console.log("Google login failed")}
           />
-          <button
+          {/* <button
             type="submit"
             className="w-full bg-[#E43941] mt-2 hover:bg-[#C93036] text-white py-2 rounded-md cursor-pointer "
           >
             Login
-          </button> 
+          </button>  */}
           <p className="text-[13px] text-slate-50 mt-3">
             Don't have an account?{" "}
             <Link to="/sign-up" className="text-[#E43941] font-medium">
