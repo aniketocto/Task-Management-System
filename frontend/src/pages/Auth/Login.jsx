e23725581534ab17616be7289a67cabca494ac1a
Aniket Khambal <aniketocto@gmail.com>
2025-08-16 10:44:13 +0530

    commented

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -116,20 +116,20 @@ const Login = () => {
         </p>
 
         <form onSubmit={handleLogin} className="mt-8">
-          <Input
+          {/* <Input
             value={email}
             onChange={({ target }) => setEmail(target.value)}
             label="Email Address"
             placeholder="example@getunstoppable.in"
             type="text"
-          /> *
+          /> 
            <Input
             value={password}
             onChange={({ target }) => setPassword(target.value)}
             label="Password"
             placeholder="Password"
             type="password"
-          />  
+          />   */}
 
           {Array.isArray(error) && error.length > 0 && (
             <ul className="text-red-500 text-sm pb-2.5">
